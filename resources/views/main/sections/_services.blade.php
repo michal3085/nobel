395672f63a2d1357e407ef7d0ae2af6b8e7e7de8
Michal Broszkiewicz <michal3085@gmail.com>
2024-09-04 20:20:29 +0200

    main-page

--- resources/views/main/sections/_services.blade.php
+++ resources/views/main/sections/_services.blade.php
@@ -3,14 +3,13 @@
         <div class="pricing-wrapper position-relative mt-n22 mt-md-n24">
             <div class="shape bg-dot primary rellax w-16 h-18" data-rellax-speed="1" style="top: 2rem; right: -2.4rem;"></div>
             <div class="shape rounded-circle bg-line red rellax w-18 h-18 d-none d-lg-block" data-rellax-speed="1" style="bottom: 0.5rem; left: -2.5rem;"></div>
-            <div class="row gx-3 gy-6 mt-3 mt-md-5 justify-content-between">
-                <div class="col-6 col-lg d-flex">
+            <div class="row gx-3 gy-6 mt-3 mt-md-5 justify-content-center">
+                <div class="col-12 col-md-6 col-lg-4 d-flex mb-4 mb-lg-0">
                     <div class="pricing card shadow-xl text-center h-100 w-100 d-flex flex-column">
                         <div class="card-body d-flex flex-column">
                             <div class="mb-auto">
                                 <img src="{{asset('/assets/img/icons/lineal/briefcase-2.svg')}}" class="svg-inject icon-svg icon-svg-md text-primary mb-3" alt="" />
                                 <h4 class="card-title">GOTOWE SPÓŁKI</h4>
-                                <br>
                                 <div class="prices text-dark">
                                     <div class="price price-show"><span class="price-currency">OD</span><span class="price-value">1500</span> <span class="price-duration">PLN</span></div>
                                 </div>
@@ -21,7 +20,7 @@
                     </div>
                 </div>
 
-                <div class="col-6 col-lg d-flex">
+                <div class="col-12 col-md-6 col-lg-4 d-flex mb-4 mb-lg-0">
                     <div class="pricing card shadow-xl text-center h-100 w-100 d-flex flex-column">
                         <div class="card-body d-flex flex-column">
                             <div class="mb-auto">
@@ -37,7 +36,7 @@
                     </div>
                 </div>
 
-                <div class="col-6 col-lg d-flex">
+                <div class="col-12 col-md-6 col-lg-4 d-flex mb-4 mb-lg-0">
                     <div class="pricing card shadow-xl text-center h-100 w-100 d-flex flex-column">
                         <div class="card-body d-flex flex-column">
                             <div class="mb-auto">
@@ -52,7 +51,7 @@
                     </div>
                 </div>
 
-                <div class="col-6 col-lg d-flex">
+                <div class="col-12 col-md-6 col-lg-4 d-flex mb-4 mb-lg-0">
                     <div class="pricing card shadow-xl text-center h-100 w-100 d-flex flex-column">
                         <div class="card-body d-flex flex-column">
                             <div class="mb-auto">
@@ -67,7 +66,8 @@
                     </div>
                 </div>
 
-                <div class="col-6 col-lg d-flex">
+
+                <div class="col-12 col-md-6 col-lg-4 d-flex mb-4 mb-lg-0">
                     <div class="pricing card shadow-xl text-center h-100 w-100 d-flex flex-column">
                         <div class="card-body d-flex flex-column">
                             <div class="mb-auto">
