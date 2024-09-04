0bb849d840bd4afefaac5bb9e31bbaf1cedee0fd
Michal Broszkiewicz <michal3085@gmail.com>
2024-09-04 20:11:43 +0200

    main-page

--- resources/views/main/sections/_services.blade.php
+++ resources/views/main/sections/_services.blade.php
@@ -4,12 +4,13 @@
             <div class="shape bg-dot primary rellax w-16 h-18" data-rellax-speed="1" style="top: 2rem; right: -2.4rem;"></div>
             <div class="shape rounded-circle bg-line red rellax w-18 h-18 d-none d-lg-block" data-rellax-speed="1" style="bottom: 0.5rem; left: -2.5rem;"></div>
             <div class="row gx-3 gy-6 mt-3 mt-md-5 justify-content-between">
-                <div class="col-6 col-lg">
-                    <div class="pricing card shadow-xl text-center h-100">
-                        <div class="card-body d-flex flex-column justify-content-between">
-                            <div>
+                <div class="col-6 col-lg d-flex">
+                    <div class="pricing card shadow-xl text-center h-100 w-100 d-flex flex-column">
+                        <div class="card-body d-flex flex-column">
+                            <div class="mb-auto">
                                 <img src="{{asset('/assets/img/icons/lineal/briefcase-2.svg')}}" class="svg-inject icon-svg icon-svg-md text-primary mb-3" alt="" />
                                 <h4 class="card-title">GOTOWE SPÓŁKI</h4>
+                                <br>
                                 <div class="prices text-dark">
                                     <div class="price price-show"><span class="price-currency">OD</span><span class="price-value">1500</span> <span class="price-duration">PLN</span></div>
                                 </div>
@@ -20,10 +21,10 @@
                     </div>
                 </div>
 
-                <div class="col-6 col-lg">
-                    <div class="pricing card shadow-xl text-center h-100">
-                        <div class="card-body d-flex flex-column justify-content-between">
-                            <div>
+                <div class="col-6 col-lg d-flex">
+                    <div class="pricing card shadow-xl text-center h-100 w-100 d-flex flex-column">
+                        <div class="card-body d-flex flex-column">
+                            <div class="mb-auto">
                                 <img src="{{asset('/assets/img/icons/lineal/briefcase-2.svg')}}" class="svg-inject icon-svg icon-svg-md text-primary mb-3" alt="" />
                                 <h4 class="card-title">WIRTUALNE BIURO</h4>
                                 <div class="prices text-dark">
@@ -36,10 +37,10 @@
                     </div>
                 </div>
 
-                <div class="col-6 col-lg">
-                    <div class="pricing card shadow-xl text-center h-100">
-                        <div class="card-body d-flex flex-column justify-content-between">
-                            <div>
+                <div class="col-6 col-lg d-flex">
+                    <div class="pricing card shadow-xl text-center h-100 w-100 d-flex flex-column">
+                        <div class="card-body d-flex flex-column">
+                            <div class="mb-auto">
                                 <img src="{{asset('/assets/img/icons/lineal/handshake.svg')}}" class="svg-inject icon-svg icon-svg-md text-primary mb-3" alt="" />
                                 <h4 class="card-title">Obsługa Prawna</h4>
                                 <div class="prices text-dark">
@@ -51,10 +52,10 @@
                     </div>
                 </div>
 
-                <div class="col-6 col-lg">
-                    <div class="pricing card shadow-xl text-center h-100">
-                        <div class="card-body d-flex flex-column justify-content-between">
-                            <div>
+                <div class="col-6 col-lg d-flex">
+                    <div class="pricing card shadow-xl text-center h-100 w-100 d-flex flex-column">
+                        <div class="card-body d-flex flex-column">
+                            <div class="mb-auto">
                                 <img src="{{asset('/assets/img/icons/lineal/calculator.svg')}}" class="svg-inject icon-svg icon-svg-md text-primary mb-3" alt="" />
                                 <h4 class="card-title">Obsługa Księgowa</h4>
                                 <div class="prices text-dark">
@@ -66,10 +67,10 @@
                     </div>
                 </div>
 
-                <div class="col-6 col-lg">
-                    <div class="pricing card shadow-xl text-center h-100">
-                        <div class="card-body d-flex flex-column justify-content-between">
-                            <div>
+                <div class="col-6 col-lg d-flex">
+                    <div class="pricing card shadow-xl text-center h-100 w-100 d-flex flex-column">
+                        <div class="card-body d-flex flex-column">
+                            <div class="mb-auto">
                                 <img src="{{asset('/assets/img/icons/lineal/calculator.svg')}}" class="svg-inject icon-svg icon-svg-md text-primary mb-3" alt="" />
                                 <h4 class="card-title">Obsługa Księgowa</h4>
                                 <div class="prices text-dark">
