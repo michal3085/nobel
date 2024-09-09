18f253cf46bd52c2e384257bf8536430d12e6cc1
Michal Broszkiewicz <michal3085@gmail.com>
2024-09-09 23:30:40 +0200

    services

--- resources/views/_layout.blade.php
+++ resources/views/_layout.blade.php
@@ -20,8 +20,9 @@
     <!--     MENU    -->
         @include('_menu')
     <!--    /MENU   -->
-
-    @include('components.movieBanner')
+    @if ($mainMenu === 'dashboard')
+        @include('components.movieBanner')
+    @endif
     <section class="wrapper bg-light ">
         <div class="container py-14 py-md-16">
             @yield('content')
