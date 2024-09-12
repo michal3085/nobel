7bf7796a69c4a8ac46f4966213fc3dc24fb8bcab
Michal Broszkiewicz <michal3085@gmail.com>
2024-09-13 01:37:09 +0200

    services

---
+++ resources/views/Services/mail/virtualOffice.blade.php
@@ -0,0 +1,7 @@
+Prośba o kontakt w sprawie: {{$data['vo_topic']}}<br><br>
+
+Kontakt do osoby wysyłającej zgłoszenie: {{$data['vo_contact']}}<br><br>
+
+
+Wiadomość:<br><br>
+{{$data['vo_message']}}
