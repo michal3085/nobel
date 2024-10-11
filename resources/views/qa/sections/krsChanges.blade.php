4d160d3b4b474d75b2f46503bb80fe3728303bc7
Michal Broszkiewicz <michal3085@gmail.com>
2024-10-11 23:52:21 +0200

    services

---
+++ resources/views/qa/sections/krsChanges.blade.php
@@ -0,0 +1,112 @@
+<h5 class="display-6 mb-3 text-center">- ZMIANY KRS -</h5>
+<div class="row">
+    <div class="col-lg-6 mb-0">
+        <div id="accordion-1" class="accordion-wrapper">
+            <div class="card accordion-item">
+                <div class="card-header" id="accordion-heading-krs1">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-krs1" aria-expanded="false" aria-controls="accordion-collapse-krs1">Co to jest KRS i dlaczego wprowadzenie zmian w tym rejestrze jest ważne?</button>
+                </div>
+                <div id="accordion-collapse-krs1" class="collapse" aria-labelledby="accordion-heading-krs1" data-bs-target="#accordion-1">
+                    <div class="card-body">
+                        <p>Krajowy Rejestr Sądowy (KRS) to centralny rejestr publiczny, w którym gromadzone są dane dotyczące m.in. spółek, stowarzyszeń, fundacji oraz innych podmiotów prawnych. Wprowadzenie zmian w KRS jest ważne, ponieważ zapewnia aktualność danych.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card accordion-item">
+                <div class="card-header" id="accordion-heading-krs2">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-krs2" aria-expanded="false" aria-controls="accordion-collapse-krs2">Jakie zmiany w spółce wymagają aktualizacji danych w KRS?</button>
+                </div>
+                <div id="accordion-collapse-krs2" class="collapse" aria-labelledby="accordion-heading-krs2" data-bs-target="#accordion-1">
+                    <div class="card-body">
+                        <p>Zmiany takie jak zmiana adresu siedziby, nazwy spółki, składu zarządu, wysokości kapitału zakładowego, kody PKD, czy zmiana wspólników muszą być zaktualizowane w KRS.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card accordion-item">
+                <div class="card-header" id="accordion-heading-krs3">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-krs3" aria-expanded="false" aria-controls="accordion-collapse-krs3">Jakie są konsekwencje niewprowadzenia zmian w KRS?</button>
+                </div>
+                <div id="accordion-collapse-krs3" class="collapse" aria-labelledby="accordion-heading-krs3" data-bs-target="#accordion-1">
+                    <div class="card-body">
+                        <p>Niewprowadzenie zmian w terminie może skutkować grzywną, odpowiedzialnością zarządu za nieaktualne dane oraz problemami w działalności spółki.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card accordion-item">
+                <div class="card-header" id="accordion-heading-krs4">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-krs4" aria-expanded="false" aria-controls="accordion-collapse-krs4">Jakie dokumenty są potrzebne do zgłoszenia zmian w KRS?</button>
+                </div>
+                <div id="accordion-collapse-krs4" class="collapse" aria-labelledby="accordion-heading-krs4" data-bs-target="#accordion-1">
+                    <div class="card-body">
+                        <p>Do zgłoszenia zmian w KRS potrzebny jest wniosek KRS-Z3, uchwały zgromadzenia wspólników lub zarządu, zaktualizowana umowa spółki oraz załączniki, np. KRS-ZM, KRS-ZK.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card accordion-item">
+                <div class="card-header" id="accordion-heading-krs5">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-krs5" aria-expanded="false" aria-controls="accordion-collapse-krs5">Ile kosztuje zgłoszenie zmiany w KRS?</button>
+                </div>
+                <div id="accordion-collapse-krs5" class="collapse" aria-labelledby="accordion-heading-krs5" data-bs-target="#accordion-1">
+                    <div class="card-body">
+                        <p>Koszt zgłoszenia zmiany to opłata sądowa (250 zł) oraz opłata za ogłoszenie w Monitorze Sądowym (100 zł). Mogą również wystąpić koszty usług prawnych lub notarialnych.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-6">
+        <div id="accordion-2" class="accordion-wrapper">
+            <div class="card accordion-item">
+                <div class="card-header" id="accordion-heading-krs6">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-krs6" aria-expanded="false" aria-controls="accordion-collapse-krs6">Jak długo trwa proces wprowadzenia zmian w KRS?</button>
+                </div>
+                <div id="accordion-collapse-krs6" class="collapse" aria-labelledby="accordion-heading-krs6" data-bs-target="#accordion-2">
+                    <div class="card-body">
+                        <p>Czas trwania procesu zależy od obciążenia sądu rejestrowego i rodzaju zmiany. Zwykle trwa od kilku tygodni do kilku miesięcy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card accordion-item">
+                <div class="card-header" id="accordion-heading-krs7">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-krs7" aria-expanded="false" aria-controls="accordion-collapse-krs7">Czy zmiany w KRS można zgłosić online?</button>
+                </div>
+                <div id="accordion-collapse-krs7" class="collapse" aria-labelledby="accordion-heading-krs7" data-bs-target="#accordion-2">
+                    <div class="card-body">
+                        <p>Tak, można zgłaszać zmiany online za pomocą systemu S24 lub Portalu Rejestrów Sądowych, korzystając z podpisu elektronicznego lub profilu zaufanego ePUAP.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card accordion-item">
+                <div class="card-header" id="accordion-heading-2-3">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-2-3" aria-expanded="false" aria-controls="accordion-collapse-2-3">Kto jest odpowiedzialny za zgłoszenie zmian w KRS?</button>
+                </div>
+                <div id="accordion-collapse-2-3" class="collapse" aria-labelledby="accordion-heading-2-3" data-bs-target="#accordion-2">
+                    <div class="card-body">
+                        <p>Za zgłoszenie zmian odpowiedzialny jest zarząd spółki lub organ uprawniony do jej reprezentacji. Brak zgłoszenia zmian może skutkować odpowiedzialnością prawną.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card accordion-item">
+                <div class="card-header" id="accordion-heading-krs9">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-krs9" aria-expanded="false" aria-controls="accordion-collapse-krs9">Czy każda zmiana w spółce musi być zgłoszona do KRS?</button>
+                </div>
+                <div id="accordion-collapse-krs9" class="collapse" aria-labelledby="accordion-heading-krs9" data-bs-target="#accordion-2">
+                    <div class="card-body">
+                        <p>Tylko zmiany, które mają wpływ na dane wpisane do KRS (np. adres, zarząd, kapitał zakładowy), muszą być zgłoszone. Zmiany wewnętrzne, które nie wpływają na dane rejestrowe, nie muszą być zgłaszane.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card accordion-item">
+                <div class="card-header" id="accordion-heading-krs10">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-krs10" aria-expanded="false" aria-controls="accordion-collapse-krs10">Jakie są najczęstsze błędy popełniane podczas zgłaszania zmian w KRS?</button>
+                </div>
+                <div id="accordion-collapse-krs10" class="collapse" aria-labelledby="accordion-heading-krs10" data-bs-target="#accordion-2">
+                    <div class="card-body">
+                        <p>Do najczęstszych błędów należy brak podpisu elektronicznego, niekompletne dokumenty, nieprawidłowe formularze, czy opóźnienie w zgłoszeniu zmian.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
