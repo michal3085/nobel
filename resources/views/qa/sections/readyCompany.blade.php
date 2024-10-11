d9b16b9fa9fd4ae7fdc4df4475a509cda5a3fd8a
Michal Broszkiewicz <michal3085@gmail.com>
2024-10-11 23:14:21 +0200

    services

--- resources/views/qa/sections/readyCompany.blade.php
+++ resources/views/qa/sections/readyCompany.blade.php
@@ -3,11 +3,11 @@
     <div class="col-lg-6 mb-0">
         <div id="accordion-1" class="accordion-wrapper">
             <div class="card accordion-item">
-                <div class="card-header" id="accordion-heading-1-1">
-                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-1-1" aria-expanded="false" aria-controls="accordion-collapse-1-1">Co to jest gotowa spółka i dlaczego warto ją kupić?</button>
+                <div class="card-header" id="accordion-heading-rc1">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-rc1" aria-expanded="false" aria-controls="accordion-collapse-rc1">Co to jest gotowa spółka i dlaczego warto ją kupić?</button>
                 </div>
                 <!-- /.card-header -->
-                <div id="accordion-collapse-1-1" class="collapse" aria-labelledby="accordion-heading-1-1" data-bs-target="#accordion-1">
+                <div id="accordion-collapse-rc1" class="collapse" aria-labelledby="accordion-heading-rc1" data-bs-target="#accordion-1">
                     <div class="card-body">
                         <p>Gotowa spółka to wcześniej zarejestrowana firma, która nie prowadziła działalności gospodarczej. Zakup takiej spółki pozwala na natychmiastowe rozpoczęcie działalności, co jest korzystne dla osób, które chcą szybko wejść na rynek, unikając formalności związanych z zakładaniem nowej firmy od podstaw.</p>
                     </div>
@@ -17,11 +17,11 @@
             </div>
             <!-- /.card -->
             <div class="card accordion-item">
-                <div class="card-header" id="accordion-heading-1-2">
-                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-1-2" aria-expanded="false" aria-controls="accordion-collapse-1-2">Jakie są korzyści z zakupu gotowej spółki?</button>
+                <div class="card-header" id="accordion-heading-rc2">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-rc2" aria-expanded="false" aria-controls="accordion-collapse-rc2">Jakie są korzyści z zakupu gotowej spółki?</button>
                 </div>
                 <!-- /.card-header -->
-                <div id="accordion-collapse-1-2" class="collapse" aria-labelledby="accordion-heading-1-2" data-bs-target="#accordion-1">
+                <div id="accordion-collapse-rc2" class="collapse" aria-labelledby="accordion-heading-rc2" data-bs-target="#accordion-1">
                     <div class="card-body">
                         <p>Zakup gotowej spółki zapewnia szereg korzyści, takich jak oszczędność czasu na rejestrację firmy, możliwość natychmiastowego rozpoczęcia działalności oraz posiadanie firmy z historią, co może ułatwić uzyskanie kredytów lub zawieranie umów.</p>
                     </div>
@@ -31,11 +31,11 @@
             </div>
             <!-- /.card -->
             <div class="card accordion-item">
-                <div class="card-header" id="accordion-heading-1-3">
-                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-1-3" aria-expanded="false" aria-controls="accordion-collapse-1-3">Czy zakup gotowej spółki wiąże się z jakimś ryzykiem?</button>
+                <div class="card-header" id="accordion-heading-rc3">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-rc3" aria-expanded="false" aria-controls="accordion-collapse-rc3">Czy zakup gotowej spółki wiąże się z jakimś ryzykiem?</button>
                 </div>
                 <!-- /.card-header -->
-                <div id="accordion-collapse-1-3" class="collapse" aria-labelledby="accordion-heading-1-3" data-bs-target="#accordion-1">
+                <div id="accordion-collapse-rc3" class="collapse" aria-labelledby="accordion-heading-rc3" data-bs-target="#accordion-1">
                     <div class="card-body">
                         <p>Główne ryzyko związane z zakupem gotowej spółki to możliwość istnienia ukrytych zobowiązań lub długów. Dlatego ważne jest, aby dokładnie sprawdzić historię i dokumentację spółki przed zakupem oraz skorzystać z usług profesjonalistów, którzy pomogą w due diligence.</p>
                     </div>
@@ -51,11 +51,11 @@
     <div class="col-lg-6">
         <div id="accordion-2" class="accordion-wrapper">
             <div class="card accordion-item">
-                <div class="card-header" id="accordion-heading-1-4">
-                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-1-4" aria-expanded="false" aria-controls="accordion-collapse-1-4">Jak przebiega proces zakupu gotowej spółki?</button>
+                <div class="card-header" id="accordion-heading-rc4">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-rc4" aria-expanded="false" aria-controls="accordion-collapse-rc4">Jak przebiega proces zakupu gotowej spółki?</button>
                 </div>
                 <!-- /.card-header -->
-                <div id="accordion-collapse-1-4" class="collapse" aria-labelledby="accordion-heading-1-4" data-bs-target="#accordion-2">
+                <div id="accordion-collapse-rc4" class="collapse" aria-labelledby="accordion-heading-rc4" data-bs-target="#accordion-2">
                     <div class="card-body">
                         <p>Proces zakupu gotowej spółki zazwyczaj obejmuje podpisanie umowy sprzedaży udziałów, przejęcie dokumentacji spółki, zgłoszenie zmian w KRS (Krajowym Rejestrze Sądowym) oraz aktualizację danych dotyczących nowego właściciela w urzędach skarbowych i innych instytucjach. Cały proces może zająć zaledwie kilka dni.</p>
                     </div>
@@ -64,11 +64,11 @@
                 <!-- /.collapse -->
             </div>
             <div class="card accordion-item">
-                <div class="card-header" id="accordion-heading-2-1">
-                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-2-1" aria-expanded="false" aria-controls="accordion-collapse-2-1">Czy mogę zmienić nazwę i przedmiot działalności gotowej spółki po jej zakupie?</button>
+                <div class="card-header" id="accordion-heading-rc5">
+                    <button class="collapsed" data-bs-toggle="collapse" data-bs-target="#accordion-collapse-rc5" aria-expanded="false" aria-controls="accordion-collapse-rc5">Czy mogę zmienić nazwę i przedmiot działalności gotowej spółki po jej zakupie?</button>
                 </div>
                 <!-- /.card-header -->
-                <div id="accordion-collapse-2-1" class="collapse" aria-labelledby="accordion-heading-2-1" data-bs-target="#accordion-2">
+                <div id="accordion-collapse-rc5" class="collapse" aria-labelledby="accordion-heading-rc5" data-bs-target="#accordion-2">
                     <div class="card-body">
                         <p>Tak, po zakupie gotowej spółki możliwe jest wprowadzenie zmian w jej statucie, takich jak zmiana nazwy, siedziby, przedmiotu działalności czy składu zarządu. Wymaga to złożenia odpowiednich wniosków do KRS i innych instytucji, ale jest to standardowa procedura, którą można zrealizować szybko.</p>
                     </div>
