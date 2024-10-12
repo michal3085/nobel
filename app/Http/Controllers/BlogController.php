3493d5f491d047c42516d82433b01fa2515d5749
Michal Broszkiewicz <michal3085@gmail.com>
2024-10-12 19:36:47 +0200

    blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function create()
+    {
+        return view('dashboard.blog.create', $this->data);
+    }
 }
