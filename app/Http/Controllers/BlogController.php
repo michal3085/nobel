9c05af1b34d8e7f3423ef093e9bd91d30a455396
Michal Broszkiewicz <michal3085@gmail.com>
2024-10-13 23:39:31 +0200

    blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PostStoreRequest;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
 
 class BlogController extends Controller
 {
@@ -20,6 +23,19 @@ class BlogController extends Controller
 
     public function store(PostStoreRequest $request)
     {
-
+        DB::beginTransaction();
+        try {
+            $post = new Post();
+            $post->fill($request->all());
+            $post->save();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            Log::error($exception->getMessage());
+            Session::flash('error', 'Wiadomość nie została wysłana');
+            return redirect()->back();
+        }
+        DB::commit();
+        Session::flash('success', 'Post został zapisany');
+        return redirect()->route('dashboard');
     }
 }
