cfd977c41c9484b848d25918330bb862438dfac3
Michal Broszkiewicz <michal3085@gmail.com>
2024-10-16 00:34:32 +0200

    blog

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class MainPageController extends Controller
 {
     public function __construct()
@@ -12,6 +14,11 @@ class MainPageController extends Controller
     public function index()
     {
         $this->data['mainMenu'] = 'dashboard';
+        $this->data['posts'] = Post::orderBy('created_at', 'desc')->limit(4)->get();
+
+//        foreach ($this->data['posts'] as $post) {
+//            dd($post->getPostImage());
+//        }
 
         return view('main.index', $this->data);
     }
