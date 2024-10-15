7e080faac0196f3e9795aaded02acfffff97fe54
Michal Broszkiewicz <michal3085@gmail.com>
2024-10-16 01:40:59 +0200

    blog

--- resources/views/main/sections/_blog.blade.php
+++ resources/views/main/sections/_blog.blade.php
@@ -7,39 +7,43 @@
                         <h3 class="display-3 mb-0 pe-xl-10 pe-xxl-15"><span class="underline-3 style-2 yellow">Blog</span> </h3>
                     </div>
                     <div class="row isotope gx-md-8 gy-8 mb-8 mt-7">
-                        @foreach($posts as $post)
-                            <article class="item post col-md-6">
-                                <div class="card h-100 d-flex flex-column">
-                                    <figure class="card-img-top overlay overlay-1 hover-scale" style="height: 250px; overflow: hidden;">
-                                        <a href="{{route('blog.show.guest', ['post' => $post])}}">
-                                            <img src="{{$post->getPostImage()}}" alt="" class="img-fluid" style="height: 100%; width: 100%; object-fit: cover;" />
-                                        </a>
-                                        <figcaption>
-                                            <h5 class="from-top mb-0">Read More</h5>
-                                        </figcaption>
-                                    </figure>
-                                    <div class="card-body d-flex flex-column">
-                                        <div class="post-header">
-                                            <div class="post-category text-line h3">
-                                                <a href="#" class="hover" rel="category">{{$post->post_title}}</a>
+                        @if ($posts->count() > 0)
+                            @foreach($posts as $post)
+                                <article class="item post col-md-6">
+                                    <div class="card h-100 d-flex flex-column">
+                                        <figure class="card-img-top overlay overlay-1 hover-scale" style="height: 250px; overflow: hidden;">
+                                            <a href="{{route('blog.show.guest', ['post' => $post])}}">
+                                                <img src="{{$post->getPostImage()}}" alt="" class="img-fluid" style="height: 100%; width: 100%; object-fit: cover;" />
+                                            </a>
+                                            <figcaption>
+                                                <h5 class="from-top mb-0">Read More</h5>
+                                            </figcaption>
+                                        </figure>
+                                        <div class="card-body d-flex flex-column">
+                                            <div class="post-header">
+                                                <div class="post-category text-line h3">
+                                                    <a href="#" class="hover" rel="category">{{$post->post_title}}</a>
+                                                </div>
+                                            </div>
+                                            <div class="post-content flex-grow-1">
+                                                <!-- Ustawienie maksymalnej wysokości dla tekstu -->
+                                                <p class="text-break" style="height: 5em; overflow: hidden;">
+                                                    {{ Str::limit(strip_tags($post->post_text), 151) }}
+                                                </p>
                                             </div>
                                         </div>
-                                        <div class="post-content flex-grow-1">
-                                            <!-- Ustawienie maksymalnej wysokości dla tekstu -->
-                                            <p class="text-break" style="height: 5em; overflow: hidden;">
-                                                {{ Str::limit(strip_tags($post->post_text), 151) }}
-                                            </p>
+                                        <div class="card-footer mt-auto">
+                                            <ul class="post-meta d-flex mb-0">
+                                                <li class="post-date"><i class="uil uil-calendar-alt"></i><span>{{$post->created_at}}</span></li>
+                                            </ul>
                                         </div>
                                     </div>
-                                    <div class="card-footer mt-auto">
-                                        <ul class="post-meta d-flex mb-0">
-                                            <li class="post-date"><i class="uil uil-calendar-alt"></i><span>{{$post->created_at}}</span></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </article>
-                            <!-- /.post -->
-                        @endforeach
+                                </article>
+                                <!-- /.post -->
+                            @endforeach
+                        @else
+                            <p>Brak postów do wyświetlenia</p>
+                        @endif
 
                     </div>
                     <!-- /.row -->
