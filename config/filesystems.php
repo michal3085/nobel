c04b54ed7a5d7e5192590aeed41f0a36e594e5fc
Michal Broszkiewicz <michal3085@gmail.com>
2025-02-03 23:22:29 +0100

    nobel-finishing

--- config/filesystems.php
+++ config/filesystems.php
@@ -80,6 +80,16 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public/blog_files/'),
             'throw' => false,
+            'permissions' => [
+                'file' => [
+                    'public' => 0775,
+                    'private' => 0775,
+                ],
+                'dir' => [
+                    'public' => 0775,
+                    'private' => 0775,
+                ],
+            ],
         ],
 
     ],
