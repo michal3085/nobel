8074e3b25836310549c84ea14da50e950e219bbe
Michal Broszkiewicz <michal3085@gmail.com>
2025-02-04 22:15:01 +0100

    nobel-finishing

--- resources/views/office/hJ5/index.blade.php
+++ resources/views/office/hJ5/index.blade.php
@@ -20,30 +20,24 @@
                                 <img src="{{asset('assets/img/offices/hj1.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
                             </figure>
                         </div>
-                        <!--/.swiper-slide -->
                         <div class="swiper-slide">
                             <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
                                 <img src="{{asset('assets/img/offices/hj2.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
                             </figure>
                         </div>
-                        <!--/.swiper-slide -->
                         <div class="swiper-slide">
                             <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
                                 <img src="{{asset('assets/img/offices/hj3.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
                             </figure>
                         </div>
-                        <!--/.swiper-slide -->
                         <div class="swiper-slide">
                             <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
                                 <img src="{{asset('assets/img/offices/hj4.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
                             </figure>
                         </div>
                     </div>
-                    <!--/.swiper-wrapper -->
                 </div>
-                <!-- /.swiper -->
             </div>
-            <!-- /.swiper-container -->
         </div>
 
         <ul class="nav nav-tabs nav-tabs-basic mb-0 mt-2">
@@ -55,7 +49,6 @@
             </li>
         </ul>
 
-        <!-- /.nav-tabs -->
         <div class="tab-content mt-2">
             <div class="tab-pane fade show active" id="tab3-1">
                 <div class="col-lg-12">
@@ -65,20 +58,48 @@
                         Czeka na Ciebie  25 klimatyzowanych biur, które urzekają oryginalnym charakterem.
                         W okolicy znajdziesz wiele kawiarni i restauracji, które w przerwie od pracy serwują pyszny lunch.
                     </p>
-                    <!--/.row -->
                 </div>
             </div>
-            <!--/.tab-pane -->
-            <div class="tab-pane fade" id="tab3-2">
-                <div class="col-lg-12">
-                    <h3 class="display-4 mb-5 text-end">Gotowe biuro i Coworking</h3>
-                    <p class="mb-6">
-                    </p>
-                    <!--/.row -->
+        </div>
+
+        <div class="text-center mt-4">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#virtualTourModal">
+                Otwórz wirtualny spacer
+            </button>
+        </div>
+
+        <div class="modal fade" id="virtualTourModal" tabindex="-1" aria-labelledby="virtualTourModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Zamknij"></button>
+                    </div>
+                    <div class="modal-body">
+                        <iframe src="https://vision360.pl/spacery/HJ5/start.html#pano5818/135.7/-0.2/90.0" class="w-100" style="height: 600px; border: none;"></iframe>
+                    </div>
                 </div>
             </div>
-            <!--/.tab-pane -->
+        </div>
+
+        <div class="container-fluid px-md-6 mt-4">
+            <div class="row">
+                <div class="col-12">
+                    <iframe class="w-100 d-block" style="height: 450px; border:0;"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2445.925659864882!2d21.042755516067466!3d52.19022397975312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecd358459b459%3A0x623876d7ca77603f!2sNobel%20-%20biura%20do%20wynaj%C4%99cia%2C%20coworking%2C%20wirtualne%20biuro!5e0!3m2!1spl!2spl!4v1607354256104!5m2!1spl!2spl"
+                            allowfullscreen="" aria-hidden="false" tabindex="0">
+                    </iframe>
+                </div>
+            </div>
         </div>
     </section>
-
 @endsection
+
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        .modal-backdrop {
+            background-color: rgba(0, 0, 0, 0.35) !important; /* Lekkie przyciemnienie tła */
+        }
+    </style>
+@endsection
+
