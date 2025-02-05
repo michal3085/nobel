755a8643e53831150152c4bf15a00be64ee5c30a
Michal Broszkiewicz <michal3085@gmail.com>
2025-02-05 01:19:23 +0100

    nobel-finishing

--- resources/views/office/oxfordTower/index.blade.php
+++ resources/views/office/oxfordTower/index.blade.php
@@ -17,21 +17,32 @@
                     <div class="swiper-wrapper">
                         <div class="swiper-slide">
                             <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
-                                <img src="{{asset('assets/img/offices/oxford.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
+                                <img src="{{asset('assets/img/offices/oxf/1.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
                             </figure>
                         </div>
                         <!--/.swiper-slide -->
                         <div class="swiper-slide">
                             <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
-                                <img src="{{asset('assets/img/offices/ch8.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
+                                <img src="{{asset('assets/img/offices/oxf/2.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
                             </figure>
                         </div>
                         <!--/.swiper-slide -->
                         <div class="swiper-slide">
                             <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
-                                <img src="{{asset('assets/img/offices/oxford2.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
+                                <img src="{{asset('assets/img/offices/oxf/3.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
                             </figure>
                         </div>
+                        <div class="swiper-slide">
+                            <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
+                                <img src="{{asset('assets/img/offices/oxf/6.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
+                            </figure>
+                        </div>
+                        <div class="swiper-slide">
+                            <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
+                                <img src="{{asset('assets/img/offices/oxf/7.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
+                            </figure>
+                        </div>
+
                         <!--/.swiper-slide -->
                     </div>
                     <!--/.swiper-wrapper -->
@@ -74,6 +85,16 @@
                 </div>
             </div>
             <!--/.tab-pane -->
+        </div>
+        <div class="container-fluid px-md-6 mt-4">
+            <div class="row">
+                <div class="col-12">
+                    <iframe class="w-100 d-block" style="height: 450px; border:0;"
+                            src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2443.9477567338554!2d21.00104917691333!3d52.22616695794225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sOXFORD%20TOWER%20CH8%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%3Cfigure%20class%3D%22rounded%20overflow-hidden%22%20style%3D%22aspect-ratio%3A%201%2F1%3B%22%3E%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%3Cimg%20src%3D%22%7B%7Basset(&#39;assets%2Fimg%2Foffices%2Foxf%2F3.jpg&#39;)%7D%7D%22%20alt%3D%22%22%20class%3D%22img-fluid%20w-100%20h-100%20object-fit-cover%22%20%2F%3E%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%3C%2Ffigure%3E%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%3C%2Fdiv%3E!5e0!3m2!1spl!2spl!4v1738712996309!5m2!1spl!2spl"
+                            allowfullscreen="" aria-hidden="false" tabindex="0">
+                    </iframe>
+                </div>
+            </div>
         </div>
     </section>
 
