bc9fc23110c046367d0cbd1639331487a803f360
Michal Broszkiewicz <michal3085@gmail.com>
2025-02-18 23:54:47 +0100

    polityki-7

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -5,7 +5,7 @@
         <div class="container pb-3 pb-md-7">
             <div class="col-lg-12 text-center text-lg-start">
                 <h3 class="display-3 mb-0 pe-xl-10 pe-xxl-15">
-                    <span class="underline-3 style-2 yellow">Wilanów Herbu Janina 5 HJ5</span>
+                    <span class="underline-3 style-2 yellow">Kontakt</span>
                 </h3>
             </div>
             <br>
