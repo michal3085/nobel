ec8514f15912c0a75be88b5aef5610b4f331e665
Michal Broszkiewicz <michal3085@gmail.com>
2025-02-19 19:52:29 +0100

    EV-24

--- resources/views/main/sections/_services.blade.php
+++ resources/views/main/sections/_services.blade.php
@@ -3,56 +3,66 @@
         <div class="row gx-md-5 gy-5 mt-n20 mb-14 mb-md-17 justify-content-center">
             <!-- GOTOWE SPÓŁKI -->
             <div class="col-12 col-md-6 col-xl-4">
-                <div class="card shadow-lg">
-                    <div class="card-body">
-                        <img src="{{asset('/assets/img/icons/lineal/briefcase-2.svg')}}" class="svg-inject icon-svg icon-svg-sm solid-mono text-fuchsia mb-3" alt="" />
+                <div class="card shadow-lg h-100">
+                    <div class="card-body text-center d-flex flex-column align-items-center">
+                        <div class="mb-3">
+                            <img src="{{asset('/assets/img/icons/lineal/briefcase-2.svg')}}" class="svg-inject icon-svg icon-svg-sm solid-mono text-fuchsia" alt="" />
+                        </div>
                         <h4>GOTOWE SPÓŁKI</h4>
-                        <p class="mb-2">Pozwala na natychmiastowe rozpoczęcie działalności gospodarczej bez zbędnych formalności.</p>
-                        <a href="{{route('service.route.dispatch', ['service' => 'gotowe-spolki'])}}" class="more hover link-fuchsia">Poznaj ofertę</a>
+                        <p class="mb-2 flex-grow-1 text-center">Pozwala na natychmiastowe rozpoczęcie działalności gospodarczej bez zbędnych formalności.</p>
+                        <a href="{{route('service.route.dispatch', ['service' => 'gotowe-spolki'])}}" class="more hover link-fuchsia mt-auto">Poznaj ofertę</a>
                     </div>
                 </div>
             </div>
             <!-- WIRTUALNE BIURO -->
             <div class="col-12 col-md-6 col-xl-4">
-                <div class="card shadow-lg">
-                    <div class="card-body">
-                        <img src="{{asset('/assets/img/icons/lineal/briefcase-2.svg')}}" class="svg-inject icon-svg icon-svg-sm solid-mono text-violet mb-3" alt="" />
+                <div class="card shadow-lg h-100">
+                    <div class="card-body text-center d-flex flex-column align-items-center">
+                        <div class="mb-3">
+                            <img src="{{asset('/assets/img/icons/lineal/briefcase-2.svg')}}" class="svg-inject icon-svg icon-svg-sm solid-mono text-violet" alt="" />
+                        </div>
                         <h4>WIRTUALNE BIURO</h4>
-                        <p class="mb-2">Adres osobisty lub dla Twojej firmy z profesjonalnym wsparciem administracyjnym.</p>
-                        <a href="{{route('service.route.dispatch', ['service' => 'wirtualne-biuro'])}}" class="more hover link-violet">Poznaj ofertę</a>
+                        <p class="mb-2 flex-grow-1 text-center">Adres osobisty lub dla Twojej firmy z profesjonalnym wsparciem administracyjnym.</p>
+                        <a href="{{route('service.route.dispatch', ['service' => 'wirtualne-biuro'])}}" class="more hover link-violet mt-auto">Poznaj ofertę</a>
                     </div>
                 </div>
             </div>
             <!-- REJESTRACJA SPÓŁKI -->
             <div class="col-12 col-md-6 col-xl-4">
-                <div class="card shadow-lg">
-                    <div class="card-body">
-                        <img src="{{asset('/assets/img/icons/lineal/handshake.svg')}}" class="svg-inject icon-svg icon-svg-sm solid-mono text-orange mb-3" alt="" />
+                <div class="card shadow-lg h-100">
+                    <div class="card-body text-center d-flex flex-column align-items-center">
+                        <div class="mb-3">
+                            <img src="{{asset('/assets/img/icons/lineal/handshake.svg')}}" class="svg-inject icon-svg icon-svg-sm solid-mono text-orange" alt="" />
+                        </div>
                         <h4>REJESTRACJA SPÓŁKI</h4>
-                        <p class="mb-2">Profesjonalne wsparcie, aby proces był szybki i bezproblemowy.</p>
-                        <a href="{{route('service.route.dispatch', ['service' => 'rejestracja-spolki'])}}" class="more hover link-orange">Poznaj ofertę</a>
+                        <p class="mb-2 flex-grow-1 text-center">Profesjonalne wsparcie, aby proces był szybki i bezproblemowy.</p>
+                        <a href="{{route('service.route.dispatch', ['service' => 'rejestracja-spolki'])}}" class="more hover link-orange mt-auto">Poznaj ofertę</a>
                     </div>
                 </div>
             </div>
             <!-- GOTOWE BIURO -->
             <div class="col-12 col-md-6 col-xl-4">
-                <div class="card shadow-lg">
-                    <div class="card-body">
-                        <img src="{{asset('/assets/img/icons/lineal/calculator.svg')}}" class="svg-inject icon-svg icon-svg-sm solid-mono text-green mb-3" alt="" />
+                <div class="card shadow-lg h-100">
+                    <div class="card-body text-center d-flex flex-column align-items-center">
+                        <div class="mb-3">
+                            <img src="{{asset('/assets/img/icons/lineal/calculator.svg')}}" class="svg-inject icon-svg icon-svg-sm solid-mono text-green" alt="" />
+                        </div>
                         <h4>GOTOWE BIURO</h4>
-                        <p class="mb-2">W pełni wyposażona, elastyczna przestrzeń, oferująca komfort i kompleksowe wsparcie.</p>
-                        <a href="{{route('service.route.dispatch', ['service' => 'gotowe-biura'])}}" class="more hover link-green">Poznaj ofertę</a>
+                        <p class="mb-2 flex-grow-1 text-center">W pełni wyposażona, elastyczna przestrzeń, oferująca komfort i kompleksowe wsparcie.</p>
+                        <a href="{{route('service.route.dispatch', ['service' => 'gotowe-biura'])}}" class="more hover link-green mt-auto">Poznaj ofertę</a>
                     </div>
                 </div>
             </div>
             <!-- ZMIANY W KRS -->
             <div class="col-12 col-md-6 col-xl-4">
-                <div class="card shadow-lg">
-                    <div class="card-body">
-                        <img src="{{asset('/assets/img/icons/lineal/user.svg')}}" class="svg-inject icon-svg icon-svg-sm solid-mono text-green mb-3" alt="" />
+                <div class="card shadow-lg h-100">
+                    <div class="card-body text-center d-flex flex-column align-items-center">
+                        <div class="mb-3">
+                            <img src="{{asset('/assets/img/icons/lineal/user.svg')}}" class="svg-inject icon-svg icon-svg-sm solid-mono text-green" alt="" />
+                        </div>
                         <h4>ZMIANY W KRS</h4>
-                        <p class="mb-2">Wieloletnie doświadczenie z zakresu dokonywania wpisów w KRS.</p>
-                        <a href="{{route('service.route.dispatch', ['service' => 'zmiany-krs'])}}" class="more hover link-green">Poznaj ofertę</a>
+                        <p class="mb-2 flex-grow-1 text-center">Wieloletnie doświadczenie z zakresu dokonywania wpisów w KRS.</p>
+                        <a href="{{route('service.route.dispatch', ['service' => 'zmiany-krs'])}}" class="more hover link-green mt-auto">Poznaj ofertę</a>
                     </div>
                 </div>
             </div>
