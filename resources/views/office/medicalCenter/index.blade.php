fdcd778d0e0c30fdd2b478ad613ad5a2fcf881ee
Michal Broszkiewicz <michal3085@gmail.com>
2025-02-26 22:29:40 +0100

    EV-30

--- resources/views/office/medicalCenter/index.blade.php
+++ resources/views/office/medicalCenter/index.blade.php
@@ -2,46 +2,72 @@
 
 @section('content')
     <section class="wrapper bg-light">
-        <div class="container pb-3 pb-md-7">
-            <div class="col-lg-12 text-center text-lg-start">
-                <h3 class="display-3 mb-0 pe-xl-10 pe-xxl-15">
-                    <span class="underline-3 style-2 yellow">MOKOTÓW CAROLINA MEDICAL CENTER</span>
-                </h3>
+
+        <div class="container py-5">
+            <div class="row mt-6">
+                <div class="col-xl-12 mx-auto">
+                    <div class="projects-tiles">
+                        <!-- /.project -->
+                        <div class="project grid grid-view">
+                            <div class="row g-6 isotope">
+                                <div class="item col-md-6">
+                                    <div class="project-details d-flex justify-content-center flex-column">
+                                        <div class="post-header">
+                                            <div class="post-category text-yellow mb-3">Lokalizacja</div>
+                                            <h2 class="underline-3 style-2 yellow">MOKOTÓW CAROLINA MEDICAL CENTER6</h2>
+                                        </div>
+                                        <!-- /.post-header -->
+                                        <div class="post-content">
+                                            <p>
+                                                Biuro usytuowane w zacisznym i malowniczym zakątku Warszawy w pobliżu dzielnic: Wawer, Wilanów i Ursynów. Piękny widok z 12 pietra na zieloną część miasta oraz na warszawskie City ukoi Twoje zmysły. Serwisowane biura zlokalizowane w Multis Plaza to doskonała propozycja dla tych, którzy cenią pracę z dala od wielkomiejskiego szumu, jednocześnie pozostając w bliskim sąsiedztwie centrum miasta.
+                                                <br>
+                                                Na warszawskim Mokotowie czeka na Ciebie kilkanaście wyposażonych i gotowych do pracy gabinetów oraz sala konferencyjna. Miła obsługa odbierze Twoją korespondencję oraz przywita gości.
+                                                <br>
+                                                W tej lokalizacji gwarantujemy również Wirtualne Biura.
+                                            </p>
+                                        </div>
+                                        <!-- /.post-content -->
+                                    </div>
+                                    <!-- /.project-details -->
+                                </div>
+                                <!-- /.item -->
+                                <div class="item col-md-6">
+                                    <figure class="itooltip itooltip-light hover-scale rounded" title='<h5 class="mb-0">Purus Tellus Magna</h5>'><a href="{{asset('assets/img/offices/carolina/WIDOK.jpg')}}" data-glightbox="title: Purus Tellus Magna" data-gallery="project-2"> <img src="{{asset('assets/img/offices/carolina/WIDOK.jpg')}}" alt="" /></a>
+                                    </figure>
+                                </div>
+                                <!-- /.item -->
+                                <div class="item col-md-6">
+                                    <figure class="itooltip itooltip-light hover-scale rounded" title='<h5 class="mb-0">Fusce Ipsum Vestibulum</h5>'><a href="{{asset('assets/img/offices/carolina/SALKA_12P.jpg')}}" data-glightbox="title: Fusce Ipsum Vestibulum" data-gallery="project-2"> <img src="{{asset('assets/img/offices/carolina/SALKA_12P.jpg')}}" alt="" /></a>
+                                    </figure>
+                                </div>
+                                <!-- /.item -->
+                                <div class="item col-md-6">
+                                    <figure class="itooltip itooltip-light hover-scale rounded" title='<h5 class="mb-0">Condimentum Parturient Ligula</h5>'><a href="{{asset('assets/img/offices/carolina/biuro_84.2.jpg')}}" data-glightbox="title: Condimentum Parturient Ligula" data-gallery="project-2"> <img src="{{asset('assets/img/offices/carolina/biuro_84.2.jpg')}}" alt="" /></a>
+                                    </figure>
+                                </div>
+                                <!-- /.item -->
+                            </div>
+                            <!-- /.row -->
+                        </div>
+                    </div>
+                    <!-- /.projects-tiles -->
+                </div>
+                <!-- /column -->
             </div>
-            <br>
+            <!-- /.row -->
         </div>
 
 
-        <ul class="nav nav-tabs nav-tabs-basic mb-0 mt-2">
-            <li class="nav-item"> <a class="nav-link active" data-bs-toggle="tab" href="#tab3-1">Gotowe biuro</a> </li>
-            <li class="nav-item"> <a class="nav-link" data-bs-toggle="tab" href="#tab3-2">Wirtualne biuro</a> </li>
-        </ul>
-        <!-- /.nav-tabs -->
-        <div class="tab-content mt-2">
-            <div class="tab-pane fade show active" id="tab3-1">
-                <div class="col-lg-12">
-                    <h3 class="display-4 mb-5 text-end">Gotowe biuro</h3>
-                    <p class="mb-6">
-                        Biuro usytuowane w zacisznym i malowniczym zakątku Warszawy w pobliżu dzielnic: Wawer, Wilanów i Ursynów. Piękny widok z 12 pietra na zieloną część miasta oraz na warszawskie City ukoi Twoje zmysły. Serwisowane biura zlokalizowane w Multis Plaza to doskonała propozycja dla tych, którzy cenią pracę z dala od wielkomiejskiego szumu, jednocześnie pozostając w bliskim sąsiedztwie centrum miasta.
-                        <br>
-                        Na warszawskim Mokotowie czeka na Ciebie kilkanaście wyposażonych i gotowych do pracy gabinetów oraz sala konferencyjna. Miła obsługa odbierze Twoją korespondencję oraz przywita gości.
-                        <br>
-                        W tej lokalizacji gwarantujemy również Wirtualne Biura.
 
-                    </p>
-                    <!--/.row -->
-                </div>
-            </div>
+
             <!--/.tab-pane -->
-            <div class="tab-pane fade" id="tab3-2">
-                <div class="col-lg-12">
-                    <h3 class="display-4 mb-5 text-end">Wirtualne biuro</h3>
-                    <p class="mb-6">Wirtualne biuro na Mokotowie to doskonała opcja dla firm pragnących skorzystać z prestiżowego adresu w jednej z najbardziej pożądanych dzielnic Warszawy. Mokotów jest znany z doskonałej infrastruktury, bliskości do centrum miasta oraz licznych parków, co czyni go idealnym miejscem do prowadzenia działalności biznesowej.</p>
-                    <!--/.row -->
-                </div>
-            </div>
-            <!--/.tab-pane -->
-        </div>
+{{--            <div class="tab-pane fade" id="tab3-2">--}}
+{{--                <div class="col-lg-12">--}}
+{{--                    <h3 class="display-4 mb-5 text-end">Wirtualne biuro</h3>--}}
+{{--                    <p class="mb-6">Wirtualne biuro na Mokotowie to doskonała opcja dla firm pragnących skorzystać z prestiżowego adresu w jednej z najbardziej pożądanych dzielnic Warszawy. Mokotów jest znany z doskonałej infrastruktury, bliskości do centrum miasta oraz licznych parków, co czyni go idealnym miejscem do prowadzenia działalności biznesowej.</p>--}}
+{{--                    <!--/.row -->--}}
+{{--                </div>--}}
+{{--            </div>--}}
     </section>
 
 @endsection
