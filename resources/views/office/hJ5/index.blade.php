ccab66b62d66cb59af864b2c19acb962da0a2031
Michal Broszkiewicz <michal3085@gmail.com>
2025-02-26 23:08:19 +0100

    EV-30

--- resources/views/office/hJ5/index.blade.php
+++ resources/views/office/hJ5/index.blade.php
@@ -2,64 +2,57 @@
 
 @section('content')
     <section class="wrapper bg-light">
-        <div class="container pb-3 pb-md-7">
-            <div class="col-lg-12 text-center text-lg-start">
-                <h3 class="display-3 mb-0 pe-xl-10 pe-xxl-15">
-                    <span class="underline-3 style-2 yellow">Wilanów Herbu Janina 5 HJ5</span>
-                </h3>
-            </div>
-            <br>
-        </div>
 
-        <div class="container-fluid px-md-6">
-            <div class="swiper-container blog grid-view" data-margin="30" data-nav="true" data-dots="false" data-items-xxl="3" data-items-md="2" data-items-xs="1">
-                <div class="swiper">
-                    <div class="swiper-wrapper">
-                        <div class="swiper-slide">
-                            <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
-                                <img src="{{asset('assets/img/offices/hj1.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
-                            </figure>
-                        </div>
-                        <div class="swiper-slide">
-                            <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
-                                <img src="{{asset('assets/img/offices/hj2.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
-                            </figure>
-                        </div>
-                        <div class="swiper-slide">
-                            <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
-                                <img src="{{asset('assets/img/offices/hj3.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
-                            </figure>
-                        </div>
-                        <div class="swiper-slide">
-                            <figure class="rounded overflow-hidden" style="aspect-ratio: 1/1;">
-                                <img src="{{asset('assets/img/offices/hj4.jpg')}}" alt="" class="img-fluid w-100 h-100 object-fit-cover" />
-                            </figure>
+        <div class="container py-5">
+            <div class="row mt-6">
+                <div class="col-xl-12 mx-auto">
+                    <div class="projects-tiles">
+                        <!-- /.project -->
+                        <div class="project grid grid-view">
+                            <div class="row g-6 isotope">
+                                <div class="item col-md-6">
+                                    <div class="project-details d-flex justify-content-center flex-column">
+                                        <div class="post-header">
+                                            <div class="post-category text-sky mb-3">Gotowe biuro</div>
+                                            <h2 class="underline-3 style-2 sky">Wilanów Herbu Janina 5 HJ5</h2>
+                                        </div>
+                                        <!-- /.post-header -->
+                                        <div class="post-content">
+                                            <p>
+                                                Lokalizacja położona na Błoniach Wilanowskich w Miasteczku Wilanów. Zapomnisz tutaj o zgiełku rozpędzonego miasta, a czas w biurze upłynie w spokojnej atmosferze.
+                                                Czeka na Ciebie  25 klimatyzowanych biur, które urzekają oryginalnym charakterem.
+                                                W okolicy znajdziesz wiele kawiarni i restauracji, które w przerwie od pracy serwują pyszny lunch.
+                                            </p>
+                                        </div>
+                                        <!-- /.post-content -->
+                                    </div>
+                                    <!-- /.project-details -->
+                                </div>
+                                <!-- /.item -->
+                                <div class="item col-md-6">
+                                    <figure class="itooltip itooltip-light hover-scale rounded" title='<h5 class="mb-0">Purus Tellus Magna</h5>'><a href="{{asset('assets/img/offices/HJ5/HJ5-above-nobel-2.jpg')}}" data-glightbox="title: Purus Tellus Magna" data-gallery="project-2"> <img src="{{asset('assets/img/offices/HJ5/HJ5-above-nobel-2.jpg')}}" alt="" /></a>
+                                    </figure>
+                                </div>
+                                <!-- /.item -->
+                                <div class="item col-md-6">
+                                    <figure class="itooltip itooltip-light hover-scale rounded" title='<h5 class="mb-0">Fusce Ipsum Vestibulum</h5>'><a href="{{asset('assets/img/offices/HJ5/HJ5-U05-nobel-1.jpg')}}" data-glightbox="title: Fusce Ipsum Vestibulum" data-gallery="project-2"> <img src="{{asset('assets/img/offices/HJ5/HJ5-U05-nobel-1.jpg')}}" alt="" /></a>
+                                    </figure>
+                                </div>
+                                <!-- /.item -->
+                                <div class="item col-md-6">
+                                    <figure class="itooltip itooltip-light hover-scale rounded" title='<h5 class="mb-0">Condimentum Parturient Ligula</h5>'><a href="{{asset('assets/img/offices/HJ5/HJ5-U04-nobel-11.jpg')}}" data-glightbox="title: Condimentum Parturient Ligula" data-gallery="project-2"> <img src="{{asset('assets/img/offices/HJ5/HJ5-U04-nobel-11.jpg')}}" alt="" /></a>
+                                    </figure>
+                                </div>
+                                <!-- /.item -->
+                            </div>
+                            <!-- /.row -->
                         </div>
                     </div>
+                    <!-- /.projects-tiles -->
                 </div>
+                <!-- /column -->
             </div>
-        </div>
-
-        <ul class="nav nav-tabs nav-tabs-basic mb-0 mt-2">
-            <li class="nav-item">
-                <a class="nav-link active" data-bs-toggle="tab" href="#tab3-1">Gotowe biuro</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="javascript:void(0);" tabindex="-1" aria-disabled="true">Wirtualne biuro</a>
-            </li>
-        </ul>
-
-        <div class="tab-content mt-2">
-            <div class="tab-pane fade show active" id="tab3-1">
-                <div class="col-lg-12">
-                    <h3 class="display-4 mb-5 text-end">Gotowe biuro</h3>
-                    <p class="mb-6">
-                        Lokalizacja położona na Błoniach Wilanowskich w Miasteczku Wilanów. Zapomnisz tutaj o zgiełku rozpędzonego miasta, a czas w biurze upłynie w spokojnej atmosferze.
-                        Czeka na Ciebie  25 klimatyzowanych biur, które urzekają oryginalnym charakterem.
-                        W okolicy znajdziesz wiele kawiarni i restauracji, które w przerwie od pracy serwują pyszny lunch.
-                    </p>
-                </div>
-            </div>
+            <!-- /.row -->
         </div>
 
         <div class="text-center mt-4">
