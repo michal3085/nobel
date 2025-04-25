80744f946e66bb1e5949e62d3354b6bbe44a9a34
Michal Broszkiewicz <michal3085@gmail.com>
2025-04-25 09:57:41 +0200

    changes

--- resources/views/office/piekna/index.blade.php
+++ resources/views/office/piekna/index.blade.php
@@ -14,14 +14,14 @@
                                     <div class="project-details d-flex justify-content-center flex-column">
                                         <div class="post-header">
                                             <div class="post-category text-yellow mb-3">Lokalizacja</div>
-                                            <h2 class="underline-3 style-2 yellow">Śródmieście - Piękna 15</h2>
+                                            <h2 class="underline-3 style-2 yellow">Śródmieście Piękna 15</h2>
                                         </div>
                                         <!-- /.post-header -->
                                         <div class="post-content">
                                             <p style="text-align: justify">
-                                                Oferujemy do wynajęcia przestronny i nowoczesny lokal biurowy o powierzchni 73 m²,
-                                                zlokalizowany w jednej z najbardziej prestiżowych lokalizacji Warszawy – przy ulicy Pięknej
-                                                15. To idealne miejsce dla firm, które cenią sobie elegancję, wygodę i dostęp do serca stolicy.
+                                                Przestronny i <b>nowoczesny</b> lokal biurowy o powierzchni 73 m²,
+                                                zlokalizowany w jednej z najbardziej <b>prestiżowych lokalizacji Warszawy – przy ulicy Pięknej
+                                                15</b>. To idealne miejsce dla firm, które cenią sobie elegancję, wygodę i dostęp do serca stolicy.
                                             </p>
                                             <div class="d-flex justify-content-center fs-9" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">
                                                 <span>
