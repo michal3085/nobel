87f57912bb922a1a6045713021b61c6f770701f9
Michal Broszkiewicz <michal3085@gmail.com>
2025-04-30 16:05:06 +0200

    newsletter

--- resources/views/dashboard/_menu.blade.php
+++ resources/views/dashboard/_menu.blade.php
@@ -18,7 +18,7 @@
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Pages</a>
                                 <ul class="dropdown-menu">
-                                    <li class="nav-item"><a class="dropdown-item" href="/pricing.html">Blog</a></li>
+                                    <li class="nav-item"><a class="dropdown-item" href="{{ route('dashboard') }}">Blog</a></li>
                                     <li class="dropdown dropdown-submenu dropend"><a class="dropdown-item dropdown-toggle" href="#" data-bs-toggle="dropdown">Newsletter</a>
                                         <ul class="dropdown-menu">
                                             <li class="nav-item"><a class="dropdown-item" href="{{ route('newsletter.subscriber.index') }}">Subskrybenci</a></li>
