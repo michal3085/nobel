c1341df11cceb5f90cfe078a3a2fef62c1c9c4f4
Michal Broszkiewicz <michal3085@gmail.com>
2025-05-07 11:38:20 +0200

    favicon-auto-select

--- resources/views/_layout.blade.php
+++ resources/views/_layout.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="An impressive and flawless site template that includes various UI elements and countless features, attractive ready-made blocks and rich pages, basically everything you need to create a unique and professional website.">
-    <meta name="keywords" content="bootstrap 5, business, corporate, creative, gulp, marketing, minimal, modern, multipurpose, one page, responsive, saas, sass, seo, startup, html5 template, site template">
+    <meta name="description" content="Nobel.pro - Profesjonalne doradztwo korporacyjne, rejestracja spółek, wirtualne biuro w Warszawie, wynajem biur, obsługa księgowa i szkolenia biznesowe. Oferujemy wsparcie administracyjne, outsourcing biura oraz gotowe spółki dla firm. Zadbaj o profesjonalny wizerunek swojej firmy z Nobel.pro.">
+    <meta name="keywords" content="doradztwo korporacyjne, rejestracja spółek, wirtualne biuro Warszawa, wynajem biura, obsługa księgowa, sekretariat telefoniczny, szkolenia biznesowe, komunikacja medialna, negocjacje, obsługa administracyjna, outsourcing biura, gotowe spółki, prestiżowy adres, wsparcie dla przedsiębiorców, usługi biznesowe">
     <meta name="author" content="elemis">
     <title>NOBEL - Gotowe Rozwiązania</title>
 
