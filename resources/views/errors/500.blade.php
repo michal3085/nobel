1f0d763c29cb41fa78acd560d5fd3d233557afdf
Michal Broszkiewicz <michal3085@gmail.com>
2025-05-08 14:12:00 +0200

    NOB-39

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,18 @@
-@extends('errors::minimal')
+@extends('_layout')
 
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Server Error'))
+@section('content')
+    <section class="wrapper bg-light">
+        <!-- /.nav-tabs -->
+        <div class="tab-content mt-2">
+            <div class="tab-pane fade show active" id="tab3-1">
+                <div class="text-center">
+                    <h1 class="display-1 fw-bold text-danger">500</h1>
+                    <h2 class="mb-4">Wewnętrzny błąd serwera</h2>
+                    <p class="mb-5">Wystąpił nieoczekiwany problem. Spróbuj ponownie później lub skontaktuj się z administratorem.</p>
+                    <a class="btn btn-primary rounded-pill" href="{{ route('main') }}">Powrót do strony głównej</a>
+                </div>
+                <!--/.tab-pane -->
+            </div>
+        </div>
+    </section>
+@endsection
