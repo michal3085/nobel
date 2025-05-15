006ad4f6987db2212dfad9ba040c0a80228a5502
Michal Broszkiewicz <michal3085@gmail.com>
2025-05-15 15:57:42 +0200

    NOB-41

--- resources/views/dashboard/blog/_form.blade.php
+++ resources/views/dashboard/blog/_form.blade.php
@@ -1,10 +1,9 @@
-
 <div class="row mb-2">
     <div class="col-lg-12">
         <div class="row mb-2">
             <label class="col-12 col-form-label col-form-label-sm">Tytuł</label>
             <div class="col-12">
-                <input class="form-control form-control-sm @error('post_title') is-invalid @enderror" type="text" name="post_title" value="{{old('post_title', $formObject->post_title)}}">
+                <input class="form-control form-control-sm @error('post_title') is-invalid @enderror" type="text" name="post_title" value="{{ old('post_title', $formObject->post_title) }}">
             </div>
         </div>
     </div>
@@ -15,27 +14,31 @@
             <label class="col-12 col-form-label col-form-label-sm">Status</label>
             <div class="col-12">
                 <select class="form-select" name="post_active">
-                    <option value="true">Aktywny</option>
-                    <option value="false">Nieaktywny</option>
+                    <option value="true" @if(old('post_active', $formObject->post_active) === 'true') selected @endif>Aktywny</option>
+                    <option value="false" @if(old('post_active', $formObject->post_active) === 'false') selected @endif>Nieaktywny</option>
                 </select>
             </div>
         </div>
     </div>
 </div>
+
 <div class="row align-items-center mb-4">
     <div class="col-lg-6">
         <label class="form-label small fw-semibold d-block mb-2">Miniaturka</label>
-        @if($formObject->post_image)
-            <img
-                src="{{ $formObject->getPostImage() }}"
-                alt="Obecna miniaturka"
-                class="img-thumbnail rounded"
-                style="max-height: 150px; width: auto;"
-            >
-            <p class="text-muted small mt-1 mb-0">Obecna miniaturka</p>
-        @else
-            <p class="text-muted small mb-0">Brak obecnej miniaturki</p>
-        @endif
+
+        {{-- Obrazek miniaturki: --}}
+        <img
+            id="thumbnailPreview"
+            src="{{ $formObject->post_image ? $formObject->getPostImage() : '' }}"
+            alt="Miniaturka"
+            class="img-thumbnail rounded"
+            style="max-height: 150px; width: auto; {{ $formObject->post_image ? '' : 'display:none;' }}"
+        >
+
+        {{-- Minimalny placeholder gdy brak obrazka, ukryty: --}}
+        <p id="noThumbnail" class="text-muted small mb-0" style="{{ $formObject->post_image ? 'display:none;' : '' }}">
+            Brak obecnej miniaturki
+        </p>
     </div>
 
     <div class="col-lg-6">
@@ -53,7 +56,6 @@
     </div>
 </div>
 
-
 <div class="row mb-2">
     <div class="col-lg-12">
         <div class="row mb-2">
@@ -64,3 +66,19 @@
         </div>
     </div>
 </div>
+
+<script>
+    document.getElementById('post_image').onchange = e => {
+        const preview = document.getElementById('thumbnailPreview');
+        const noThumb = document.getElementById('noThumbnail');
+        if (!e.target.files[0]) {
+            preview.style.display = 'none';
+            noThumb.style.display = 'block';
+            preview.src = '';
+            return;
+        }
+        preview.src = URL.createObjectURL(e.target.files[0]);
+        preview.style.display = 'inline-block';
+        noThumb.style.display = 'none';
+    }
+</script>
