3311d70bc1adda25948ef26605d3bfd9f474a028
Michal Broszkiewicz <michal3085@gmail.com>
2025-08-21 13:33:15 +0200

    NOB-changes

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -5,9 +5,10 @@
         <div class="container pb-3 pb-md-7">
             <div class="col-lg-12 text-center text-lg-start">
                 <h3 class="display-3 mb-0 pe-xl-10 pe-xxl-15">
-                    <span class="underline-3 style-2 yellow">Kontakt</span>
+                    <span class="">Kontakt</span>
                 </h3>
             </div>
+            <br>
             <br>
         </div>
         <div class="container pb-3 pb-md-7">
